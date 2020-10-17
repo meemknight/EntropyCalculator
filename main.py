c70812f4af2a8dc1b3fc06e866c949718bba06ca
vlod <vladalex420@gmail.com>
2020-10-17 21:23:25 +0300

    tasks

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #!python3 
+
+#todo (Vlod): add a function to read all the data from available pdf documents
+def readData():
+    None
+
+#todo (Vlod): count letters capply the formula
+def calculateEntropyLetters(data):
+    None
